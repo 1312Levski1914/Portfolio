d77bb0e077b3d82fce3bdbc6455005abbb1046d7
Levski <krushkov1914@gmail.com>
2021-12-20 00:32:01 +0100

    mobile done
    ad

--- javaScript.js
+++ javaScript.js
@@ -42,32 +42,168 @@ myAudio.onplaying = function() {
 myAudio.onpause = function() {
   isPlaying = false;
 };
-
-function drop(){
-  let arr = document.getElementsByClassName('show-skill');
-  let arr1 = document.getElementsByClassName('hide');
+function toTop(){
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+//
+function dropContact(){
+  let arr1 = document.getElementsByClassName('dropContactInfo');
   let display;
-  for(let i = 0; i<arr.length; i++){
-    display= arr[i].style.display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
     if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function dropProjects(){
+  let arr1 = document.getElementsByClassName('showProjects');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showStudying(){
+  let arr1 = document.getElementsByClassName('showStudyingInfo');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showDesigner(){
+  let arr1 = document.getElementsByClassName('designerHide');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showScript(){
+  let arr1 = document.getElementsByClassName('showScriptSkills');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showGraphic(){
+  let arr1 = document.getElementsByClassName('showGraphicSkills');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showCertificate(){
+  let arr1 = document.getElementsByClassName('certificateSkills');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showAbout(){
+  let arr1 = document.getElementsByClassName('showAboutInfo');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
+function showVideoSkills(){
+  let arr1 = document.getElementsByClassName('videoSkills');
+  let display;
+  for(i = 0 ; i<arr1.length ; i++){
+    display = arr1[i].style.display;
+    var arg = arr1[i];
+    if(display !== 'block'){
+      arg.style.display = 'block';
+    }else{
+      arg.style.display = 'none';
+    }
+  }
+}
 
+function showLife(){
+  let arr = document.getElementsByClassName('LifeSection');
+  let display;
+
+  for(let i = 0 ; i<arr.length ; i++){
+    display = arr[i].style.display;
+    if(display !== 'block'){
       arr[i].style.display = 'block';
     }else{
       arr[i].style.display = 'none';
     }
   }
-  for(i = 0 ; i<arr1.length ; i++){
-    display = arr1[i].style.display;
-    var arg = arr1[i];
-    console.log(arg);
-    
-  }
-  if(display !== 'block'){
-    arg.style.display = 'block';
-  }else{
-    arg.style.display = 'none';
+}
+function showWork(){
+  let arr = document.getElementsByClassName('showWorkInfo');
+  let display;
+
+  for(let i = 0 ; i<arr.length ; i++){
+    display = arr[i].style.display;
+    if(display !== 'block'){
+      arr[i].style.display = 'block';
+    }else{
+      arr[i].style.display = 'none';
+    }
   }
 }
-function toTop(){
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+
+function showSkillsMenu(){
+  let arr = document.getElementsByClassName('bearShowSskills');
+  let display;
+  for(let i = 0 ; i<arr.length ; i++){
+    display = arr[i].style.display;
+    if(display !== 'block'){
+      arr[i].style.display = 'block';
+      document.getElementById('LION').style.display = 'none';
+      document.getElementById('deerID').style.display = 'none';
+    }else{
+      arr[i].style.display = 'none';
+      document.getElementById('LION').className = 'lion';
+    }
+  }
+  
 }
